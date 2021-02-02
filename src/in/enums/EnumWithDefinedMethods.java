ee653d5a486dea4feb74e3ba48f116f6a820c0d1
Sarat Chandra Sekhar Ryali <sarat.ryali@planonsoftware.com>
2021-02-03 00:46:49 +0530

    added search method

--- src/in/enums/EnumWithDefinedMethods.java
+++ src/in/enums/EnumWithDefinedMethods.java
@@ -13,29 +13,6 @@ public class EnumWithDefinedMethods {
 	}
 	
 	public void printOption(Options aOption) {
-		switch (aOption) {
-		case ONE :
-			aOption.action();
-			break;
-		case TWO:
-			aOption.action();
-			break;
-		case THREE:
-			aOption.action();
-			break;
-		}
+		aOption.action();
 	}
-	
-	public void one() {
-		System.out.println("one");
-	}
-	
-	public void two() {
-		System.out.println("two");
-	}
-	
-	public void three() {
-		System.out.println("three");
-	}
-
 }
---
+++ src/in/other/PrintClassesInPackage.java
@@ -0,0 +1,81 @@
+package in.other;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public class PrintClassesInPackage {
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+//		Class[] classes = getClasses("in.other");
+		Class[] classes = getClasses("in.files");
+		for(Class cls:classes) {
+			System.out.println(cls.getSimpleName());
+		}
+		searchForFile("TextFileWrite", classes);
+	}
+
+	/**
+	 * Scans all classes accessible from the context class loader which belong to
+	 * the given package and subpackages.
+	 *
+	 * @param packageName The base package
+	 * @return The classes
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	private static Class[] getClasses(String packageName) throws ClassNotFoundException, IOException {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		assert classLoader != null;
+		String path = packageName.replace('.', '/');
+		Enumeration resources = classLoader.getResources(path);
+		List<File> dirs = new ArrayList();
+		while (resources.hasMoreElements()) {
+			URL resource = (URL) resources.nextElement();
+			dirs.add(new File(resource.getFile()));
+		}
+		ArrayList classes = new ArrayList();
+		for (File directory : dirs) {
+			classes.addAll(findClasses(directory, packageName));
+		}
+		return (Class[]) classes.toArray(new Class[classes.size()]);
+	}
+
+	/**
+	 * Recursive method used to find all classes in a given directory and subdirs.
+	 *
+	 * @param directory   The base directory
+	 * @param packageName The package name for classes found inside the base
+	 *                    directory
+	 * @return The classes
+	 * @throws ClassNotFoundException
+	 */
+	private static List findClasses(File directory, String packageName) throws ClassNotFoundException {
+		List classes = new ArrayList();
+		if (!directory.exists()) {
+			return classes;
+		}
+		File[] files = directory.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				assert !file.getName().contains(".");
+				classes.addAll(findClasses(file, packageName + "." + file.getName()));
+			} else if (file.getName().endsWith(".class")) {
+				classes.add(
+						Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+			}
+		}
+		return classes;
+	}
+	
+	private static void searchForFile(String aClass, Class[] classes) {
+		System.out.println("\n\nsearching for file:: "+aClass);
+		for(Class cls: classes) {
+			if(cls.getSimpleName().contentEquals(aClass)) {
+				System.out.println(cls.getSimpleName());
+			}
+		}
+	}
+}
