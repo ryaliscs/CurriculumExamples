0bc5a2f243236e34168412cca2a62eb9eed39e88
Sarat Chandra Sekhar Ryali <sarat.ryali@planonsoftware.com>
2021-07-14 19:10:51 +0530

    code refactoring

--- src/contest/GirlFriend_02.java
+++ src/contest/GirlFriend_02.java
@@ -17,106 +17,109 @@ import java.util.Scanner;
  */
 public class GirlFriend_02 {
 
+	private static int DEBUG_LEVEL = 0; // {-1, 0, 1} '-1' for no logging; '0' for time calculation; '1' for all nodes
+										// and path traversing
+
 	public static void main(String[] args) {
-		boolean debug = true;
 		Instant start = Instant.now();
 		int destination_house = -1;
 		int noOfRoads = -1;
 
-		Map<Integer, List<PointWeight>> roadMap;
+		Map<Integer, List<Node>> roadMap;
+
 		try (Scanner sc = new Scanner(System.in)) {
 			String input_houses_roads = sc.nextLine();
 			String[] input_split = input_houses_roads.split(" ");
 			destination_house = Integer.parseInt(input_split[0]);
 			noOfRoads = Integer.parseInt(input_split[1]);
 			roadMap = getRoadMap(sc, destination_house, noOfRoads);
-//			if (debug)
-//				System.out.println(roadMap);
+
+			if (DEBUG_LEVEL > 0)
+				System.out.println(roadMap);
 		}
-		if (debug) {
-			Instant end = Instant.now();
-			Duration timeElapsed = Duration.between(start, end);
-			System.out.println("Input ->" + timeElapsed.toSeconds() + " sec");
-			start = Instant.now();
-		}
+
+		endProcessedTimeLog(start);
+		start = Instant.now();
 
 		findShortestPathCost(destination_house, roadMap);
 
-		if (debug) {
+		endProcessedTimeLog(start);
+	}
+
+	private static void endProcessedTimeLog(Instant start) {
+		if (DEBUG_LEVEL >= 0) {
 			Instant finalEnd = Instant.now();
 			Duration totalTimeElapsed = Duration.between(start, finalEnd);
-			System.out.println("\nfinal ->" + totalTimeElapsed.toSeconds() + " sec");
-			System.out.println("\nfinal ->" + totalTimeElapsed.toMillis() + " milli");
+			System.out.println("\nprocessed in -> " + totalTimeElapsed.toSeconds() + " sec");
+			System.out.println("processed in -> " + totalTimeElapsed.toMillis() + " milli");
 		}
 	}
 
-	private static void findShortestPathCost(int destination_house, Map<Integer, List<PointWeight>> roadMap) {
+	private static void findShortestPathCost(int destination_house, Map<Integer, List<Node>> roadMap) {
 		if (roadMap.isEmpty()) {
 			System.out.print("NOT POSSIBLE");
+			return;
+		}
 
-		} else {
-			Node root = new Node(1);
+		int counter = 0;
+		int shortPathCost = -1;
+		LinkedList<Node> nodes = new LinkedList<Node>();
+		nodes.add(new Node(1));
+		boolean isShortestPathFound = false;
 
-			int counter = 0;
+		Map<Integer, Integer> nodeWeights = new HashMap<Integer, Integer>(roadMap.size());
 
-			int shortPathCost = -1;
-			LinkedList<Node> nodes = new LinkedList<Node>();
-			nodes.add(root);
-			boolean isShortestPathFound = false;
+		while (!nodes.isEmpty()) {
+			Node parent = nodes.remove();
 
-			Map<Integer, Integer> nodeWeights = new HashMap<Integer, Integer>(roadMap.size());
+			if (isShortestPathFound && parent.weight >= shortPathCost) {
+				continue;
+			}
 
-			while (!nodes.isEmpty()) {
-				Node parent = nodes.remove();
-
-				if (isShortestPathFound && parent.weight >= shortPathCost) {
+			Integer currentNodeWeight = nodeWeights.get(parent.nodeValue);
+			if (currentNodeWeight != null) {
+				if (parent.weight > currentNodeWeight.intValue()) {
 					continue;
 				}
-
-				Integer currentNodeWeight = nodeWeights.get(parent.nodeValue);
-				if (currentNodeWeight != null) {
-					if (parent.weight > currentNodeWeight.intValue()) {
-						continue;
-					}
-				}
-
-				List<PointWeight> childNodes = roadMap.get(parent.nodeValue);
-				for (PointWeight pw : childNodes) {
-					Node node = new Node(pw.to, pw.weight, parent.weight);
-					if (!isValidNodeWeight(nodeWeights, node)) {
-						continue;
-					}
-					boolean isAdd = node.nodeValue > parent.nodeValue;
-					if (isAdd) {
-						if (node.nodeValue == destination_house
-								&& (shortPathCost == -1 || shortPathCost > node.weight)) {
-							shortPathCost = node.weight;
-							isShortestPathFound = true;
-							isAdd = false;
-						} else if (shortPathCost != -1 && node.weight >= shortPathCost) {
-							isAdd = false;
-						}
-						if (isAdd) {
-							nodes.add(node);
-						}
-					}
-				}
-				counter++;
-
-//				System.out.println(nodes);
-//				System.out.println("Shortest Path cost:" + shortPathCost);
-//				System.out.println("No. of iterations:" + counter);
 			}
 
-			System.out.println("No. of iterations:" + counter);
-
-			if (!isShortestPathFound) {
-				System.out.print("NOT POSSIBLE");
-			} else if (shortPathCost < 0) {
-				System.out.print("0");
-			} else {
-				System.out.print(shortPathCost);
+			List<Node> childNodes = roadMap.get(parent.nodeValue);
+			for (Node child : childNodes) {
+				Node childNode = new Node(child.nodeValue, child.weight, parent.weight);
+				if (!isValidNodeWeight(nodeWeights, childNode)) {
+					continue;
+				}
+				// check the weight of the child node if it is valid to get added to the list
+				boolean isAdd = true;
+				if (childNode.nodeValue == destination_house
+						&& (!isShortestPathFound || childNode.weight < shortPathCost)) {
+					shortPathCost = childNode.weight;
+					isShortestPathFound = true;
+					isAdd = false;
+				} else if (isShortestPathFound && childNode.weight >= shortPathCost) {
+					isAdd = false;
+				}
+				if (isAdd) {
+					nodes.add(childNode);
+				}
 			}
+			counter++;
+			if (DEBUG_LEVEL > 0) {
+				System.out.println(nodes);
+				System.out.println("Shortest Path cost:" + shortPathCost);
+				System.out.println();
+			}
+		}
+
+		if (DEBUG_LEVEL >= 0) {
+			System.out.println("\nNo. of iterations:" + counter);
+			System.out.println();
+		}
+
+		if (!isShortestPathFound) {
+			System.out.print("NOT POSSIBLE");
+		} else {
+			System.out.print(shortPathCost);
 		}
 	}
 
@@ -133,21 +136,6 @@ public class GirlFriend_02 {
 		}
 		return false;
 
-	}
-
-	private static class PointWeight implements Comparable<PointWeight> {
-		public int to;
-		public int weight;
-
-		@Override
-		public String toString() {
-			return to + " (" + weight + ")";
-		}
-
-		@Override
-		public int compareTo(PointWeight aPW) {
-			return aPW.weight - weight;
-		}
 	}
 
 	private static class Node {
@@ -173,26 +161,27 @@ public class GirlFriend_02 {
 
 		@Override
 		public String toString() {
-			String result = nodeValue + " (" + weight + ")\n";
+			String result = nodeValue + " (" + weight + ")";
 
 			return result;
 		}
 	}
 
-	private static Map<Integer, List<PointWeight>> getRoadMap(Scanner sc, int destinationHouse, int noOfRoads) {
-		Map<Integer, List<PointWeight>> mapRoadMap = new HashMap<>(destinationHouse);
+	private static Map<Integer, List<Node>> getRoadMap(Scanner sc, int destinationHouse, int noOfRoads) {
+		Map<Integer, List<Node>> mapRoadMap = new HashMap<>(destinationHouse);
 		boolean hasStopHouse = false;
 		boolean hasStartHouse = false;
+
 		for (int i = 0; i < noOfRoads; i++) {
+
 			String input_path_weight = sc.nextLine();
 			String[] input_split = input_path_weight.split(" ");
+
 			int from = Integer.parseInt(input_split[0]);
 			int to = Integer.parseInt(input_split[1]);
 			int weight = Integer.parseInt(input_split[2]);
 
-			boolean isFromEqualsToDestination = from == destinationHouse;
-			boolean isToEqualsToDestination = to == destinationHouse;
-			if (isFromEqualsToDestination || isToEqualsToDestination) {
+			if ((from == destinationHouse) || (to == destinationHouse)) {
 				hasStopHouse = true;
 			}
 
@@ -205,10 +194,13 @@ public class GirlFriend_02 {
 			updateRouteMap(mapRoadMap, to, from, weight); // b -> a
 		}
 
+		// if there no start or stop node then there is no path from start to stop
 		if (!hasStopHouse && !hasStartHouse) {
 			return Collections.emptyMap();
 		}
 
+		// if there are no children for the start or stop node then there is no path
+		// from start to stop
 		if (mapRoadMap.get(destinationHouse) == null || //
 				mapRoadMap.get(Integer.valueOf(1)) == null) {
 			return Collections.emptyMap();
@@ -216,16 +208,23 @@ public class GirlFriend_02 {
 		return mapRoadMap;
 	}
 
-	private static void updateRouteMap(Map<Integer, List<PointWeight>> mapRoadMap, int from, int to, int weight) {
+	/**
+	 * updateRouteMap: updates the parent as key and children as values
+	 * 
+	 * @param mapRoadMap
+	 * @param from       path starting point
+	 * @param to         path end point
+	 * @param weight     cost to travel the path
+	 */
+	private static void updateRouteMap(Map<Integer, List<Node>> mapRoadMap, int from, int to, int weight) {
 		Integer intFrom = Integer.valueOf(from);
-		List<PointWeight> values = mapRoadMap.get(intFrom);
+		List<Node> values = mapRoadMap.get(intFrom);
 		if (values == null) {
 			values = new ArrayList<>(500);
 			mapRoadMap.put(intFrom, values);
 		}
-		PointWeight pw = new PointWeight();
-		pw.to = to;
-		pw.weight = weight;
-		values.add(pw);
+		Node node = new Node(to);
+		node.weight = weight;
+		values.add(node);
 	}
 }
